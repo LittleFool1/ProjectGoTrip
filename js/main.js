8ef04fb9bc916df57a9b5e6496509d81da300a24
LittleFool1 <77936169+LittleFool1@users.noreply.github.com>
2022-12-12 17:16:01 +0400

    commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+  const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    loop: true,
+    margin: 20,
+    startPosition: 0,
+    items: 1,
+    responsive : {
+        640 : {
+            items: 3,
+            startPosition: 1,
+        },
+        850 : {
+            items: 3,
+            startPosition: 1,
+        },
+        1200 : {
+            items: 3,
+            margin: 30,
+        }
+    }
+
+});
+$('.slider_btn-prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+})
+
+$('.slider_btn-next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
+const navBtn = document.querySelector('.nav_togle');
+const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+navBtn.onclick = function () {
+    nav.classList.toggle('nav_mobile');
+    menuIcon.classList.toggle('menu-icon-active');
+};
+
